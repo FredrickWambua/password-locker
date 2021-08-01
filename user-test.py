1951b94d6be66c26b5eff3adc7b7af1fc4fc195d
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-02 00:40:14 +0300

    defines display and user existence test methods

--- user-test.py
+++ user-test.py
@@ -31,7 +31,24 @@ class TestUser(unittest.TestCase):
         Test case to check if multiple users can be saved
         '''
         self.new_user.save_user() # method to save user
+        test_user = User('test','user','any')
+        test_user.save_user()
         self.assertEqual(len(User.user_list),4)
+
+    def test_display_users(self):
+        '''
+        test case to check if users are displayed
+        '''
+        self.assertEqual(User.display_users(),User.user_list)
+    def test_user_exists(self):
+        '''
+        test case to check if the user exists in the user list
+        '''
+        self.new_user.save_user()
+        test_user = User('test','name','password')
+        test_user.save_user()
+        user_exits = User.user_exists('test')
+        self.assertTrue(user_exits)
 
 if __name__ == '__main__':
     unittest.main()
