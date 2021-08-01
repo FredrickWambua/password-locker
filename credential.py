e258ae3059f7cfc9afd24b4611398cecd5684ce4
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-01 23:57:26 +0300

    creates a credential class and initialises it

---
+++ credential.py
@@ -0,0 +1,15 @@
+class Credential:
+    '''
+    Class that generates new instances of user credentials
+    '''
+    credential_list = []
+
+    def __init__(self, account_name, key):
+        self.account_name = account_name
+        self.key = key
+
+    def save_credentials(self):
+        '''
+        this method saves the credentials of a user when they create an account
+        '''
+        Credential.credential_list.append(self)
