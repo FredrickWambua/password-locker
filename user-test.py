ce593d05b75f523525f45e952abfe043674c3559
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-02 00:19:06 +0300

    adds tests of saving users

--- user-test.py
+++ user-test.py
@@ -19,5 +19,19 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.name, 'Fredrick')
         self.assertEqual(self.new_user.password, '19xy13!')
 
+    def test_save_user(self):
+        '''
+        Test method to test if the user has been saved on the user list
+        '''
+        self.new_user.save_user() # method to save user
+        self.assertEqual(len(User.user_list),2) #expecting 1 !=2 since there is only one contact saved currently
+
+    def test_save_many_users(self):
+        '''
+        Test case to check if multiple users can be saved
+        '''
+        self.new_user.save_user() # method to save user
+        self.assertEqual(len(User.user_list),4)
+
 if __name__ == '__main__':
     unittest.main()
