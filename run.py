b5858afb0b1fa409f82a849104ca9652e725b260
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-02 02:09:24 +0300

    creates all the required functions

--- run.py
+++ run.py
@@ -30,4 +30,34 @@ def erase_credential(credential):
     A function to delete a credential
     '''
     credential.erase_credential()
-    
+
+def check_if_user_exists(password):
+    '''
+    A function that checks if the user exists for authentification
+    '''
+    return User.user_exists(password)
+
+def find_account(name, password):
+    '''
+    A function to find an account by specifying a name
+    '''
+    return User.find_account(password)
+
+def create_user(name, password):
+    '''
+    A function that creates a new user
+    '''
+    new_user = User(name, password)
+    return new_user
+
+def save_user(user):
+    '''
+    A function to save user
+    '''
+    user.save_user()
+
+def display_users():
+    '''
+    A function that displays all the users
+    '''
+    return User.display_users()
