9be0711216d8e75a6146c34015609e4f883935e5
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-02 03:16:35 +0300

    creates loops to define the short codes and attach their functionalities

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from logging import getLevelName
 from user import User
 from credential import Credential
 import random
@@ -61,3 +62,109 @@ def display_users():
     A function that displays all the users
     '''
     return User.display_users()
+
+
+def intro():
+    print('Welcome to Password Manager')
+    print('\n')
+    print('Sign up for an account')
+
+    while True:
+        print(' Use these shoer codes : cu - Create user, lg - login if user exists, du - display users, q - Quit/ exit the app ')
+        print('-'*56)
+        print('\n')
+        short_code = input().lower()
+        print('\n')
+
+        if short_code == 'cu':
+            print('New user')
+            print('-'*8)
+
+            print('Enter your name')
+            name = input()
+
+            print('Enter your password')
+            password = input()
+            print('\n')
+
+            save_user(create_user(name, password))
+            print('\n')
+            print(f'Great! {name} you created an account \n')
+            print('\n')
+
+        elif short_code =='lg':
+            print('Enter your username')
+            account_name = input()
+            print('\n')
+
+            auth = getpass.getpass('password:')
+            if check_if_user_exists(auth):
+                search_account = find_account(auth)
+
+
+                while True:
+                    print(f'Welcome {search_account.name}, \n')
+                    print(' Use cc- To create new credentials, vc- To view all your credentials, ex- To exit the account \n ')
+                    print('-'*56)
+                    short_code = input().lower()
+                    if short_code == 'cc':
+                        print('New credential creation')
+                        print('-'*8)
+                        print('Enter account name')
+                        account_name = input()
+                        print('Create a password \n')
+                        print('To make your own password, press- a, to generate a password press- g \n')
+                        print('-'*56)
+                        generate = input()
+                        print('\n')
+
+                        if generate == 'g':
+                            letters = string.ascii_letters + string.digits
+                            generatedpassword = ''.join(random.choice(letters) for i in range(9))
+                            print(f'Your new generated pass word is: {generatedpassword} \n')
+                            key = generatedpassword
+
+                        elif generate == 'a':
+                            print("Enter its password")
+                            key = input()
+                            print('\n')
+                        print(f'{account_name} has been saved')
+
+                        save_credential(create_credential(account_name,key))
+
+                    elif short_code == 'vc':
+                        if display_credentials:
+                            print('Well, This is the list of all your accounts and passwords \n')
+                            for credential in display_credentials():
+                                print(f'Account name: {credential.account_name} - password: {credential.key}')  
+
+                    elif short_code == 'dc':
+                        print('Which credential would you like to delete?')
+                        erase_account = input()
+                        if erase_account == account_name:
+                            Credential.credential_list.remove(credential)
+                            print('Credential deleted')
+                        else:
+                            print('Credentials do not match any.')
+
+                    elif short_code == 'ex':
+                        print('You have exited your account')
+                        break
+            else:
+                print('The password was incorrect')
+                print('\n')
+        elif short_code =='du':
+            print('List of all the users \n')
+            for user in display_users():
+                print(f"{user.name} \n")
+
+        elif short_code == 'q':
+            print('You have quit the app')
+
+            break
+
+        else:
+            print('Use the specified short codes to access the service')
+
+if __name__ == '__main__':
+    intro()
